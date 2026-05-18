45b4bb37bcedf04cf530c32c88e8a72821340d0a
Torsten Raudssus <torsten@raudss.us>
2026-05-18 22:38:43 +0200

    Initial commit: Alien::Libgit2
    Provides libgit2 for CPAN modules linking via FFI or XS.
    
    - pkg-config probe for system libgit2 (>= 1.5)
    - Falls back to building from bundled libgit2-1.9.3.tar.gz
    - CMake build with USE_SSH=ON, USE_HTTPS=OpenSSL, REGEX_BACKEND=builtin
    - Test::Alien smoke test attaches git_libgit2_init/_shutdown/_version
    - No network access required at install time
    
    First piece of a new native Git stack for Perl
    (Alien::Libgit2 -> Git::Libgit2 -> Git::Native).

---
+++ lib/Alien/Libgit2.pm
@@ -0,0 +1,35 @@
+# ABSTRACT: Find or build libgit2, the linkable Git library
+
+package Alien::Libgit2;
+our $VERSION = '0.001';
+use strict;
+use warnings;
+use parent 'Alien::Base';
+
+1;
+
+=head1 SYNOPSIS
+
+  use Alien::Libgit2;
+
+  # For XS consumers
+  my $cflags = Alien::Libgit2->cflags;
+  my $libs   = Alien::Libgit2->libs;
+
+  # For FFI consumers (FFI::Platypus, Git::Libgit2)
+  my @libs = Alien::Libgit2->dynamic_libs;
+
+=head1 DESCRIPTION
+
+L<Alien::Libgit2> provides the C library L<libgit2|https://libgit2.org/>
+for use by other CPAN modules that need to link against it.
+
+It first checks whether a system C<libgit2> (>= 1.5) is available via
+C<pkg-config>. If not, it builds libgit2 from a bundled source tarball
+using CMake. No network access is required during install.
+
+=head1 SEE ALSO
+
+L<Git::Libgit2>, L<Git::Native>, L<Alien::Build>, L<Alien::Base>
+
+=cut
